b70ef5cce2864cc1f9c0f70ff513318c2eb1a53b
Ilja <ilja@ylyxa.ylyxa.com>
2020-11-21 12:07:54 +0300

    Scripted auto-commit on change (2020-11-21 12:07:54) by gitwatch.sh

--- src/main/java/GetMessage.java
+++ src/main/java/GetMessage.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class GetMessage {
+import akka.actor.AbstractActor;
+
+public class GetMessage {
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public GetMessage(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
 }
--- src/main/java/StoreMessage.java
+++ src/main/java/StoreMessage.java
@@ -1,2 +1,18 @@
+import akka.actor.AbstractActor;
+
 public class StoreMessage {
+    private String key;
+    private String value;
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public StoreMessage(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
 }
