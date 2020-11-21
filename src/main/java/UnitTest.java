6ccdeccfbe37d349a641d9d2b4b9144bbaff6828
Ilja <ilja@ylyxa.ylyxa.com>
2020-11-21 18:09:20 +0300

    Scripted auto-commit on change (2020-11-21 18:09:20) by gitwatch.sh

--- src/main/java/UnitTest.java
+++ src/main/java/UnitTest.java
@@ -1,9 +1,9 @@
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UnitTest {
     public final String packageID;
     public final String jsScript;
